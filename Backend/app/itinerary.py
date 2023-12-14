f1b30c692ca704382bc3ead22fc0d64600aad5b7
anaxz <annah704@gmail.com>
2023-12-14 17:27:22 +0000

    Add note about api key

--- Backend/app/itinerary.py
+++ Backend/app/itinerary.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from .OpenAI_API.main import createItinerary
 
 ## note2self: remember to change to python ver 9 or open ai wont work!!!
-
+# create a OPENAI_API_KEY in this folder levele and add: OPENAI_API_KEY="api key here"
 
 itinerary = Blueprint('itinerary', __name__) 
 CORS(itinerary)
@@ -22,7 +22,8 @@ def get_itinerary():
         leave_date = request_data['leaveDate'] 
         to_note = request_data['toNote'] 
 
-        default_question = "I want you to act as a travel planner. I will provide you specific details about my trip, where I'm going, when I'm going, who I'm going with, and important things to note. You will provide a specific itinerary with the vacation plan. My trip: " + "Where I'm going: " + travel_to + ". When I'm going: " + leave_date + ". Who is going: " + people + ". Important things to note: " + to_note +"."
+        # Added in here to return as json
+        default_question = "I want you to act as a travel planner. I will provide you specific details about my trip, where I'm going, when I'm going, who I'm going with, and important things to note. You will provide a specific itinerary with the vacation plan and return it as json. My trip: " + "Where I'm going: " + travel_to + ". When I'm going: " + leave_date + ". Who is going: " + people + ". Important things to note: " + to_note +"."
 
         output = createItinerary(default_question)
         # print(output)
